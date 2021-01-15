0d0cababb3016faa156b40c229b33546df3a45a3
Oleksandr Slipchenko <o.p.slipchenko@gmail.com>
2021-01-15 18:00:35 +0200

    Готовое ДЗ

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types';
 import s from './FeedbackOptions.module.css';
+import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackButton = ({ feedback, onLeaveFeedback }) => {
+  const id = uuidv4();
   return (
     <button
+      key={id}
       className={s.feedbackButton}
       type="button"
       data-feedback={feedback}
